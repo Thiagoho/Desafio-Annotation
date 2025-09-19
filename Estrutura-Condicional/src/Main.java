dd78ddd26fc617bddb520ee447228654ecf51a0f
Thiago H O S <thiago2006_sales@hotmail.com>
2025-09-19 05:38:20 -0300

    Exercícios: Sobre porcetagem do salario / imposto.

--- Estrutura-Condicional/src/Main.java
+++ Estrutura-Condicional/src/Main.java
@@ -22,25 +22,27 @@ public class Main {
 		 *  no total. O valor deve ser impresso comduas casas decimais.
 		*/
 		Locale.setDefault(Locale.US);
+		
 		Scanner sc = new Scanner(System.in);
-		
 		double salario = sc.nextDouble();
-		
 		double imposto;
+		
 		if (salario <= 2000.0) {
 			imposto = 0.0;
-		} else if (salario <= 3000.0){
-			imposto = (salario - 2000.0) * 0.08;
+		} else if (salario <= 3000.0) {
+			imposto = (salario - 2000.0) * 0.08 + 1000.0;
 		} else if (salario <= 4500.0) {
 			imposto = (salario - 3000.0) * 0.18 + 1000.0 * 0.08;
 		} else {
 			imposto = (salario - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
 		}
-		if (imposto == 0.0) {
-			System.out.println("Inseto");
+		
+		if (salario <= 2000.0) {
+			System.out.println("Insento!");
 		} else {
 			System.out.printf("R$ %.2f%n", imposto);
 		}
+		
 		sc.close();
 
 	}
