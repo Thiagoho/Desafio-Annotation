76a04a03ae42f46ab071a508e1021f9dec9f939e
Thiago H O S <thiago2006_sales@hotmail.com>
2025-09-25 05:36:45 -0300

    Exemplo com if/else.

--- Estrutura-Condicional/src/Main.java
+++ Estrutura-Condicional/src/Main.java
@@ -1,42 +1,25 @@
 
-import java.lang.invoke.SwitchPoint;
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		/*  Problema exemplo
-		 *  Fazer um programa para ler um valor
-		 *  inteiro de 1 a 7 representando um
-		 *  dia da semana (sendo 1=domingo, 2=segunda, e assim por diante). 
-		 *  Escrever na tela o dia da semana correspondente, conforme exemplos.
-		*/
+	/* Espressão condicional Ternária.
+	 * Estrutura opcional ao if-else quando se deseja decidir um VALOR com base em uma
+	 * condição.*/
 		Locale.setDefault(Locale.US);
 		
 		Scanner sc = new Scanner(System.in);
-		int semana = sc.nextInt();
-		String dia;
-		
-		if (semana == 1) {
-			dia = "Domingo.";
-		} else if (semana == 2) {
-			dia = "Segunda - feira";
-		} else if (semana == 3) {
-			dia = "Terça - feira.";
-		} else if (semana == 4) {
-			dia = "Quarta - feira.";
-		} else if (semana == 5) {
-			dia = "Quinta - feira.";
-		} else if (semana == 6) {
-			dia = "Sexta - feira.";
-		} else if (semana == 7) {
-			dia = "Sabádo.";
+		double  preco = 34.5;
+		double desconto;
+		if (preco < 20.0) {
+			desconto = preco * 0.1;
 		} else {
-			dia = "Valor inválido!";
+			desconto = preco * 0.5;
 		}
-		System.out.println("Dia da semana: " + dia);
-			
+		System.out.println("Seu desconto: "+ desconto);
 		sc.close();
 	}
 }
