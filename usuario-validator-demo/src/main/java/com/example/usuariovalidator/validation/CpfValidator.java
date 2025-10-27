ff76bfdc34ddb96fbf5042f541d4401a16c55358
Thiago H O S <135985268+Thiagoho@users.noreply.github.com>
2025-10-27 05:44:42 -0300

    Update CpfValidator.java

--- usuario-validator-demo/src/main/java/com/example/usuariovalidator/validation/CpfValidator.java
+++ usuario-validator-demo/src/main/java/com/example/usuariovalidator/validation/CpfValidator.java
@@ -1,3 +1,12 @@
+/*
+ * A annotation @Cpf usa @Constraint para ligar-se ao validador CpfValidator.
+ * O validador usa expressão regular para confirmar o formato correto.
+ * Retorna true se o campo estiver vazio (para permitir uso
+ * com @NotBlank separado) ou se o formato for válido.
+ */
+
+
+
 package com.example.usuariovalidator.validation;
 
 import jakarta.validation.ConstraintValidator;
